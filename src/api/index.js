b918e006ce8bd75272c189df5cb7937b5fe83bda
pranav_balpande <120770461+pranav4536@users.noreply.github.com>
2026-04-13 23:06:53 +0530

    add new api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://milk-mangment-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
